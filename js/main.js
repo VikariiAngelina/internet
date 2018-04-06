e8ca07ac54e64cdb4352dcc4741729fb0fa37089
VikariiAngelina <linuli4ka@gmail.com>
2018-04-06 03:26:34 +0300

    added main header

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 $(function() {
 
-	//$(window).resize(function() {
+	$(".toggleMobMenu").on("click",function(){
+		$(".header__mobMenuInner").stop().slideToggle();
+	});
+
+	$(window).resize(function() {
 	let windowWidth=$(window).width();
 
-	if (windowWidth>=992) {
+	if (windowWidth>=768) {
 		$(".topMenu").appendTo(".header__topInner");
 		$(".search").appendTo(".header__topInner");
 		$(".mainMenu").appendTo(".header__bottomLeft");
 		$(".basket").appendTo(".header__bottomInner");
-	//}	
-	}//)
+	}	
+	})
 	
 });
+
+
 
